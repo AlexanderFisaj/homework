44ed344f9c89fa9c0975bb48c726b472bea75622
FisayAA <fisay@fisay.ru>
2023-02-04 17:55:30 +0300

    lesson 02 task 13

---
+++ less02_task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+if (a < 0)
+    a = a * -1;
+
+if (a > 100)
+{
+    a = (a - a % 100) / 100 % 10;
+    Console.WriteLine($"{a}");
+}    
+else
+    Console.WriteLine("В этом числе третьей цифры нет.");
