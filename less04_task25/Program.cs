7a982c188150021526cf43056b370f7a61a6d960
FisayAA <fisay@fisay.ru>
2023-02-11 22:29:16 +0300

    lesson 04 task 25

---
+++ less04_task25/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 25: 
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Нельзя использовать Math.Pow();
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень, в которую нужно возвести Ваше число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int rez = 1;
+
+if (b == 0) rez = 1;
+
+for (int i = 0; i < b; i++)
+{
+    rez = rez * a;
+}
+
+Console.WriteLine($"Результат = {rez}");
